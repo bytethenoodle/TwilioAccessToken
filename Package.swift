d0f7599117ddb4c02615f4e0f1521dfd3a08041d
Elbert Orozco <elbertorozco@protonmail.com>
2019-07-31 19:10:32 +0800

    Update source for Swift 4.2 and above

--- Package.swift
+++ Package.swift
@@ -1,8 +1,21 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
     name: "TwilioAccessToken",
+    products: [
+        .library(name: "TwilioAccessToken", targets: ["TwilioAccessToken"])
+    ],
     dependencies: [
-      .Package(url: "https://github.com/brentschooley/JSONWebToken.swift", majorVersion: 2, minor: 0)
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/bytethenoodle/JSONWebToken.swift", .branch("master"))
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "TwilioAccessToken",
+            dependencies: ["JWT"])
     ]
 )
