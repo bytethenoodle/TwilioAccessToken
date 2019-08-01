8023e658ee014d91ff5d481f5c8848302653ede9
Elbert Orozco <elbertorozco@protonmail.com>
2019-08-01 11:13:31 +0800

    Implement initial update for Swift 5.0
    *iOS spec

--- Package.swift
+++ Package.swift
@@ -1,15 +1,20 @@
-// swift-tools-version:4.0
-
+// swift-tools-version:5.0
+//
+// Use command "swift package generate-xcodeproj --xcconfig-overrides ./Sources/ios.xcconfig" to generate for iOS
+//
 import PackageDescription
 
 let package = Package(
     name: "TwilioAccessToken",
+    platforms: [
+        .iOS(.v10),
+    ],
     products: [
         .library(name: "TwilioAccessToken", targets: ["TwilioAccessToken"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/bytethenoodle/JSONWebToken.swift", .branch("master"))
+        .package(url: "https://github.com/bytethenoodle/JSONWebToken.swift", .branch("bytethenoodle/iOS"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
